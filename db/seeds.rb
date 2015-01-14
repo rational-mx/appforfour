76c28f99b33dcd5f78bbcbd1c58bcb74a6ce2da7
Diego Llamas <diegollams@gmail.com>
2015-01-14 12:30:18 -0600

    Add seeds and show userna in index post

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+user = User.new(
+  username: "user",
+  email: "user@mail.com",
+  password:  '12345678',
+  password_confirmation: '12345678'
+)
+user.save!	
+post = Post.new(
+	body: "Este es un commet dura duran espis din weis",
+	user_id: user.id
+)
+post.save!
