579ff87c64828cf52a54c3096d43d84ae00e3b7c
Diego Llamas <diegollams@gmail.com>
2015-02-24 23:42:16 -0600

    correct for new post with no comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,13 @@ class Post < ActiveRecord::Base
   has_one :device_inf
   has_many :notifications, as: :notificable
   def comment_for_show_in_post( number_of_comments)
-    self.comments.order('id desc').all[number_of_comments-4..number_of_comments].reverse
+
+    comments =   self.comments.order('id desc').all[number_of_comments-4..number_of_comments]
+    if comments
+      comments
+    else
+      []
+    end
   end
 
 
