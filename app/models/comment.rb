0ab5c80cc2d9112ddcac908d9e3976efb8739b2e
Diego Llamas <diegollams@gmail.com>
2015-02-24 23:59:56 -0600

    add dependet to commets and post with devise_infs

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,6 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
   has_many :likes, as: :likeable
-  has_one :device_inf
+  has_one :device_inf, dependent: :destroy
   has_many :notifications, as: :notificable
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,9 @@
 class Post < ActiveRecord::Base
   belongs_to :user
   belongs_to :medium, polymorphic: true
-  has_many :comments
+  has_many :comments ,dependent: :destroy
   has_many :likes, as: :likeable
-  has_one :device_inf
+  has_one :device_inf, dependent:  :destroy
   has_many :notifications, as: :notificable
   def comment_for_show_in_post( number_of_comments)
 
